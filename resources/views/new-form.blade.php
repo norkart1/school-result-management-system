d7c77d090718bfe924e0d473f718563438aa5971
Salman MP <mpsalman2011@gmail.com>
2024-09-21 17:45:05 +0530

    minor edits

--- resources/views/new-form.blade.php
+++ resources/views/new-form.blade.php
@@ -3,12 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Results Form</title>
+    <title>Exam Results - Suffa Dars Coordination</title>
 
     <!-- CSS link or use inline style -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&family=Raleway:wght@300;400;700&display=swap" rel="stylesheet">
-
-      
 
     <style>
 

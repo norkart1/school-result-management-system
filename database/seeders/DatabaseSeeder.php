08a64bbc53929f6bc322c7a8ea2e410ac07eaf28
Salman MP <mpsalman2011@gmail.com>
2024-09-23 09:56:39 +0530

    minor bug fixes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,13 +11,13 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // Comment this out if you're not seeding users
+        // $this->call(UsersTableSeeder::class);
+        
+        // Only call StudentsTableSeeder if you want to seed students
+        $this->call(StudentsTableSeeder::class);
     }
+    
 }

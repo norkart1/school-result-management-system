fb4829f5c85b7e64b7e3bd910d88611853eb22e2
captain nork <138743746+norkart1@users.noreply.github.com>
2025-09-16 16:13:32 +0000

    Update test

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css', // Default app.css
+                'resources/css/app.css', // Defaugglt app.css
                 'resources/css/style.css', // Add your custom style.css
                 'resources/js/app.js'
             ],
